1458fb41c9372743b8a63d12700673034837af89
Akyza <bryce.tavares@gmail.com>
2017-11-22 10:37:21 +0100

    Modification

--- Model/RapportVisiteManager.class.php
+++ Model/RapportVisiteManager.class.php
@@ -6,11 +6,22 @@
  * Date: 13/11/2017
  * Time: 20:16
  */
+
+
 class RapportVisiteManager
 {
+	// Ajoute un rapport de visite dans la base de donnée
 	public static function addRapport(RapportVisite $rapport){
-		$query = MonPdo::getInstance()->prepare('INSERT INTO rapport_visite()');
+//		$query = MonPdo::getInstance()->prepare('INSERT INTO rapport_visite()');
 	}
+
+	// Retourne la liste de tout les rapport de visite
+	public static function getLstRapport(){
+
+		$query = MonPdo::getInstance()->query('SELECT * FROM rapport_visite');
+		return $query->fetchAll(PDO::FETCH_CLASS|PDO::FETCH_PROPS_LATE, 'RapportVisite');
+	}
+<<<<<<< HEAD
 	public static function updRapport($rapNum, $visitMat, $praCode, $rapDate, $rapBilan, $rapMotif, $medDepLeg1, $medDepLeg2){
         $query = MonPdo::getInstance()->prepare('UPDATE rapport_visite SET visiteurMatricule = :visitMat, praCode = :praCode, rapDate = :rapDate, rapBilan = :rapBilan, rapMotif = :rapMotif, medDepotLegal1 = :medDepLeg1, medDepotLegal2 = :medDepLeg2 WHERE rapNum = ?');
         $query -> execute(array(
@@ -24,3 +35,14 @@ class RapportVisiteManager
     ));
     }
 }
+=======
+
+	// Retourne la liste des rapport pour une équipe
+	public static function getLstRapportEquipe($idEquipe){
+		// il faut d'abord boucler sur chaque personne de la liste
+//		$query = MonPdo::getInstance()->query("SELECT * FROM ")
+	}
+
+
+}
+>>>>>>> origin/master
